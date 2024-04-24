8373c7831b2959ccc040499e6fb10bd9ede1f0fa
TWODAFT <skylarsmoke@gmail.com>
2024-04-23 23:31:28 -0700

    Window Layout Updates
    The Send button has been moved closer to it's final location and event handlers have been created to handle button events

--- RumorClient_Desktop/MainWindow.cpp
+++ RumorClient_Desktop/MainWindow.cpp
@@ -2,38 +2,98 @@
 #include <wx/wx.h>
 #include <wx/splitter.h>
 
+
+
+/// <summary>
+/// Creates the send button on the message type panel
+/// </summary>
+/// <param name="msgTypePanel">Message Type Panel</param>
+/// <returns></returns>
+wxButton* CreateSendButton(wxPanel* msgTypePanel) {
+	// Send Button
+	wxButton* sendMsgButton = new wxButton(msgTypePanel, -1, wxT("Send"), wxDefaultPosition, wxDefaultSize, wxBORDER_NONE);
+	sendMsgButton->SetBackgroundColour(wxColor(50, 50, 50));
+	sendMsgButton->SetForegroundColour(wxColor(200, 200, 200));
+	
+	return sendMsgButton;
+}
+
+/// <summary>
+/// Creates the Chat Panel
+/// </summary>
+/// <param name="currentFrame">Current wxFrame</param>
+/// <returns></returns>
+wxPanel* CreateChatPanel(wxFrame *currentFrame) {
+	// panel that contains chats created
+	wxPanel* chatPanel = new wxPanel(currentFrame, wxID_ANY, wxDefaultPosition, wxSize(100, 100));
+	chatPanel->SetBackgroundColour(wxColor(30, 30, 30));
+	chatPanel->SetMaxSize(wxSize(300, 5000));
+	return chatPanel;
+}
+
+/// <summary>
+/// Creates the Message Panel
+/// </summary>
+/// <param name="currentFrame">Current wxFrame</param>
+/// <returns></returns>
+wxPanel* CreateMessagePanel(wxFrame* currentFrame) {
+	// panel that contains messages for current chat
+	wxPanel* msgPanel = new wxPanel(currentFrame, wxID_ANY, wxDefaultPosition, wxSize(500, 600));
+	msgPanel->SetBackgroundColour(wxColor(40, 40, 40));
+	return msgPanel;
+}
+
+/// <summary>
+/// Creates the message type panel
+/// </summary>
+/// <param name="currentFrame">Current wxFrame</param>
+/// <returns></returns>
+wxPanel* CreateMessageTypePanel(wxFrame* currentFrame) {
+	// panel that allows the typing of a message
+	wxPanel* msgTypePanel = new wxPanel(currentFrame, wxID_ANY, wxDefaultPosition, wxSize(500, 50));
+	msgTypePanel->SetBackgroundColour(wxColor(33, 33, 33));
+	return msgTypePanel;
+}
+
 // main window GUI logic
 MainWindow::MainWindow(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(nullptr, wxID_ANY, title, pos, size) {
 
-	// panel that contains chats created
-	wxPanel* chatPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(100,100));
-	chatPanel->SetBackgroundColour(wxColor(30, 30, 30));
-	chatPanel->SetMaxSize(wxSize(300,5000));
-	
-	// panel that contains messages for current chat
-	wxPanel* msgPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(500, 600));
-	msgPanel->SetBackgroundColour(wxColor(40, 40, 40));
+	// panel that contains chats
+	wxPanel* chatPanel = CreateChatPanel(this);
+
+	// panel that contains messages
+	wxPanel* msgPanel = CreateMessagePanel(this);
 
 	// panel that allows the typing of a message
-	wxPanel* msgTypePanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(500, 50));
-	msgTypePanel->SetBackgroundColour(wxColor(33, 33, 33));
+	wxPanel* msgTypePanel = CreateMessageTypePanel(this);
 
-	// Send Button
-	wxButton* sendMsgButton = new wxButton(msgTypePanel, -1, wxT("Send"));
+	// send button
+	wxButton* sendMsgButton = CreateSendButton(msgTypePanel);
+	// send button handlers
+	sendMsgButton->Bind(wxEVT_ENTER_WINDOW, &MainWindow::SendButtonHover, this);
+	sendMsgButton->Bind(wxEVT_LEAVE_WINDOW, &MainWindow::SendButtonLeave, this);
+	sendMsgButton->Bind(wxEVT_BUTTON, &MainWindow::SendButtonClick, this);
 
-	// panel sizers
+	// chat side sizer
 	wxBoxSizer* leftSizer = new wxBoxSizer(wxHORIZONTAL);
 	leftSizer->Add(chatPanel, 1, wxEXPAND | wxLEFT);
 
+	// msg side sizers
 	wxBoxSizer* rightSizer = new wxBoxSizer(wxVERTICAL);
 	rightSizer->Add(msgPanel, 1, wxEXPAND | wxALL);
 	rightSizer->Add(msgTypePanel, 0, wxEXPAND | wxALL);
 
 	leftSizer->Add(rightSizer, 1, wxEXPAND | wxALL);
 
+	// send button sizer
+	wxBoxSizer* sendSizer = new wxBoxSizer(wxVERTICAL);
+	msgTypePanel->SetSizer(sendSizer);
+	sendSizer->Add(sendMsgButton, 0, wxALL | wxALIGN_RIGHT);
+
 	this->SetSizerAndFit(leftSizer);
 }
 
+// destructor
 MainWindow::~MainWindow() {
 
 }
--- RumorClient_Desktop/MainWindow.h
+++ RumorClient_Desktop/MainWindow.h
@@ -8,6 +8,24 @@ public:
 	MainWindow(const wxString& title,
 			   const wxPoint& pos,
 			   const wxSize& size);
+
+	// send button hover event handler
+	void SendButtonHover(wxMouseEvent& event) {
+		this->SetBackgroundColour(wxColor(120, 20, 10));
+		this->Refresh();
+	}
+
+	void SendButtonLeave(wxMouseEvent& event) {
+		this->SetBackgroundColour(wxColor(50, 50, 50));
+		this->Refresh();
+	}
+
+	void SendButtonClick(wxCommandEvent& event) {
+		SetBackgroundColour(wxColor(300, 300, 300));
+		Refresh();
+		Update();
+	}
+
 	~MainWindow();
 
 };
